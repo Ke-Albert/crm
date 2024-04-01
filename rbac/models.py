76f1933d631c95e9d3eca63a2d269ea760447cd6
kk <kkforever66@gmail.com>
2024-04-01 21:51:49 +0800

    Create basic crm model

---
+++ rbac/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here.
+class UserInfo(models.Model):
+    """用户表"""
+    name = models.CharField(verbose_name='name', max_length=32)
+    password=models.CharField(verbose_name='pwd',max_length=64)
+    email=models.CharField(verbose_name='email',max_length=32)
+    roles=models.ManyToManyField(verbose_name='owned roles',to='Role',blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Permission(models.Model):
+    """权限表"""
+    title=models.CharField(verbose_name='title',max_length=32)
+    url=models.URLField(verbose_name='reg url',max_length=128)
+
+    def __str__(self):
+        return self.title
+
+class Role(models.Model):
+    """角色表"""
+    role=models.CharField(verbose_name='role',max_length=32)
+    permissions=models.ManyToManyField(verbose_name='owned permissions',to='Permission',blank=True)
+
+    def __str__(self):
+        return self.role
---
+++ web/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from web.views import customer,payment
+
+urlpatterns = [
+
+    path('customer/list/', customer.customer_list,),
+    path('customer/add/', customer.customer_add,),
+    path('customer/edit/<int:cid>/', customer.customer_edit,),
+    path('customer/del/<int:cid>/', customer.customer_del),
+    path('customer/import/', customer.customer_import),
+    path('customer/tpl/', customer.customer_tpl),
+
+    path('payment/list/', payment.payment_list),
+    path('payment/add/', payment.payment_add),
+    path('payment/edit/<int:pid>/', payment.payment_edit),
+    path('payment/del/<int:pid>/', payment.payment_del),
+
+]
